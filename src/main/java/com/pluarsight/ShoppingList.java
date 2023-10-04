f5fe997ae9e8d85e0fa7d677119eaa8b0c65c3c8
Jada Harris <107131999+JadeZinHarris@users.noreply.github.com>
2023-10-04 09:40:03 -0400

    ShoppingList part 1

---
+++ src/main/java/com/pluarsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluarsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Apples");
+        System.out.println("Grapes");
+        System.out.println("Pears");
+        System.out.println("Deli Meat");
+        System.out.println("Cheese");
+        System.out.println("Eggs");
+        System.out.println("Soad");
+        System.out.println("Water");
+        System.out.println("Soap");
+        System.out.println("Oil");
+    }
+}
